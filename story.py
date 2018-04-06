641ea0f12ad454b7688747c8c3167a9b295eb1fc
Dylan Barva <d.barva5402@gmail.com>
2018-04-06 15:23:54 -0700

    4/6/18, added enums for branch type

--- story.py
+++ story.py
@@ -1,5 +1,5 @@
 import time
-
+import enum
 
 class Story(object):
 
@@ -14,7 +14,8 @@ class Story(object):
         if len(self.author) > 0:
             print('By ' + self.author)
         print('This is an interactive story. Type the number before the option to select the option.')
-        self.enter_branch(1)
+        start = self.get_start()
+        self.enter_branch(start.id)
 
     def enter_branch(self, id):
         available_choices = []
@@ -24,7 +25,7 @@ class Story(object):
         print(working_branch.trunk)
         time.sleep(0.25)
         print(working_branch.text)
-        if not working_branch.end:
+        if not working_branch.type == Type.END:
             for x in range(len(working_branch.choice_list)):
                 id_list = working_branch.get_choice(x)
                 available_choices.append(str(id_list[0]))
@@ -37,16 +38,21 @@ class Story(object):
                 choice = str(choice)
                 if choice in available_choices:
                     done = True
-                    print(choice, working_branch.id)
+                    print(choice)
                     self.enter_branch(int(choice))
                 else:
                     print('The choice is invalid, please try again.')
-        elif working_branch.end:
+        elif working_branch.type == Type.END:
             self.end()
 
     def get_branch_by_id(self, id):
         for x in self.branch_list:
             if x.check_id(id):
+                return x
+
+    def get_start(self):
+        for x in self.branch_list:
+            if x.type == Type.START:
                 return x
 
     def end(self):
@@ -63,21 +69,23 @@ class Story(object):
             print('Would you like to play again? Type Y for yes and N for no.')
 
 
+class Type(enum.Enum):
+    START = 1
+    END = 2
+    MIDDLE = 3
+
+
 class Branch(object):
 
-    def __init__(self, id, trunk, text, choice_list = [], end=False):
+    def __init__(self, id, trunk, text, choice_list, type = Type.MIDDLE):
         self.id = id
         self.trunk = trunk
         self.text = text
         self.choice_list = choice_list
-        self.end = end
+        self.type = type
 
     def get_choice(self, x):
         return self.choice_list[x]
-        # print()
-
-    def get_trunk(self):
-        return self.trunk
 
     def check_id(self, id):
         if id == self.id:
@@ -87,14 +95,13 @@ class Branch(object):
 
 
 branch_ = Branch(1678, '', '', [(971, ''), (118, '')])#template for branches
-branch_1 = Branch(1, 'hi', 'What will you say?', [(2, 'bye'), (3, 'hello')])
+branch_1 = Branch(1, 'hi', 'What will you say?', [(2, 'bye'), (3, 'hello'), (8, 'banana')], Type.START)
 branch_2 = Branch(2, 'bye', 'What will you eat', [(4, 'ice cream'), (5, 'sandwich')])
 branch_3 = Branch(3, 'what', 'ABCD', [(6, 'good night'), (7, 'morning!')])
 branch_4 = Branch(4, 'I like ice cream', 'pick a number', [(8, 'eight'), (9, 'nine')])
-branch_8 = Branch(8, 'It\'s time for the end', 'THE END', [(),()], True)
+branch_8 = Branch(8, 'It\'s time for the end', 'THE END', [], Type.END)
 
 list = [branch_1, branch_2, branch_3, branch_4, branch_8]
-taco = Story('Test story', list, 'bob')
+# taco = Story('Test story', list, 'bob')
 taco = Story('Test story', list)
-
-print(taco.start())
+taco.start()
